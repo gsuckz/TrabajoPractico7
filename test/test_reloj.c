3655d6368eac4c616b902b06626ab3706a963dbb
Jesus Avila <jesus.avila.05@gmail.com>
2023-06-14 23:44:07 -0300

    prueba2

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -25,16 +25,16 @@ void test_reloj_init(void){
 }
 
 
-//void test_reloj_corriendo(){
-//    uint8_t ESPERADO[6] = {1,0,2,0,0,0};
-//    uint8_t RESULTADO[6];
-//    Reloj * reloj = relojInit(alarma);
-//    relojConfig(reloj,0,0,0);
+void test_reloj_corriendo(){
+    uint8_t ESPERADO[6] = {1,0,2,0,0,0};
+    uint8_t RESULTADO[6];
+    Reloj * reloj = relojInit(alarma);
+    relojConfig(reloj,0,0,0);
 //    for (double i=0; i<=TICKS; i++) relojTick(reloj);
 //    relojTime(reloj,RESULTADO);
 //    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO,RESULTADO,6);
 //    relojKill(reloj);
-//}
+}
 
 
 //void test_alarm_set(){
