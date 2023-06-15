742c38ca88974746fb776a316b140fe026199ba7
Jesus Avila <jesus.avila.05@gmail.com>
2023-06-15 00:11:29 -0300

    descomentado

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -30,43 +30,43 @@ void test_reloj_corriendo(){
     uint8_t RESULTADO[6];
     Reloj * reloj = relojInit(alarma);
     relojConfig(reloj,0,0,0);
-//    for (double i=0; i<=TICKS; i++) relojTick(reloj);
-//    relojTime(reloj,RESULTADO);
-//    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO,RESULTADO,6);
-//    relojKill(reloj);
+    for (double i=0; i<=TICKS; i++) relojTick(reloj);
+    relojTime(reloj,RESULTADO);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO,RESULTADO,6);
+    relojKill(reloj);
 }
 
 
-//void test_alarm_set(){
-//    Reloj * reloj = relojInit(alarma);
-//    alarma(0);
-//    relojConfig(reloj,0,0,0);
-//    for (double i=0; i<=TICKS; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(0,alarmacheck());
-//    relojAlarmConfig(reloj,9,25);
-//    for (double i=0; i<=HORA; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(1,alarmacheck());
-//    relojAlOff(reloj);
-//    TEST_ASSERT_EQUAL(0,alarmacheck());
-//    relojKill(reloj);
-//}   
-//
-//
-//void test_snooze(){
-//    Reloj * reloj = relojInit(alarma);
-//    alarma(0);
-//    relojConfig(reloj,0,0,0);
-//    for (double i=0; i<=TICKS; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(0,alarmacheck());
-//    relojAlarmConfig(reloj,9,25);
-//    for (double i=0; i<=HORA; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(1,alarmacheck());
-//    relojSnooze(reloj,5);
-//    for (double i=0; i<=2*MINUTO; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(0,alarmacheck());
-//    for (double i=0; i<=3*MINUTO; i++) relojTick(reloj);
-//    TEST_ASSERT_EQUAL(1,alarmacheck());
-//    relojAlOff(reloj);
-//    TEST_ASSERT_EQUAL(0,alarmacheck());
-//    relojKill(reloj);
-//}
+void test_alarm_set(){
+    Reloj * reloj = relojInit(alarma);
+    alarma(0);
+    relojConfig(reloj,0,0,0);
+    for (double i=0; i<=TICKS; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(0,alarmacheck());
+    relojAlarmConfig(reloj,9,25);
+    for (double i=0; i<=HORA; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(1,alarmacheck());
+    relojAlOff(reloj);
+    TEST_ASSERT_EQUAL(0,alarmacheck());
+    relojKill(reloj);
+}   
+
+
+void test_snooze(){
+    Reloj * reloj = relojInit(alarma);
+    alarma(0);
+    relojConfig(reloj,0,0,0);
+    for (double i=0; i<=TICKS; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(0,alarmacheck());
+    relojAlarmConfig(reloj,9,25);
+    for (double i=0; i<=HORA; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(1,alarmacheck());
+    relojSnooze(reloj,5);
+    for (double i=0; i<=2*MINUTO; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(0,alarmacheck());
+    for (double i=0; i<=3*MINUTO; i++) relojTick(reloj);
+    TEST_ASSERT_EQUAL(1,alarmacheck());
+    relojAlOff(reloj);
+    TEST_ASSERT_EQUAL(0,alarmacheck());
+    relojKill(reloj);
+}
